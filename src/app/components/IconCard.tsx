51741158b68760c9c202946294a30d105dd5d427
Aakash Thotakura <aakashthotakura@gmail.com>
2025-12-01 19:29:06 +0530

    added aspect-ratio to make the icon look circular

--- src/app/components/IconCard.tsx
+++ src/app/components/IconCard.tsx
@@ -15,9 +15,9 @@ export default function IconCard({ iconsArray }: IconCardProps) {
     <section style={{ maxWidth: "1200px", minWidth: "350px", margin: "2em auto", padding: "1em 0.5em", display: "flex", justifyContent: "center", flexWrap: "wrap", gap: "1em", }}>
       {
         iconsArray.map((entry, id) => (
-          <div key={id} style={{ textAlign: "center", border: "1px solid goldenrod", padding: "1em", borderRadius: "1em", flex: "1 1 auto", width: "550px", minWidth:"350px" }}>
-            <p style={{ display: "inline-block", fontSize: "2em", backgroundColor: "goldenrod", borderRadius: "50%", marginBottom: "0.5em", }}>{entry.icon}</p>
-            <p className="font-bold text-yellow-800" style={{fontSize:"1.2em"}}>{entry.title}</p>
+          <div key={id} style={{ textAlign: "center", border: "1px solid goldenrod", padding: "1em", borderRadius: "1em", flex: "1 1 auto", width: "550px", minWidth: "350px" }}>
+            <p style={{ width: "60px", aspectRatio: "1", display: "inline-block", placeItems: "center", placeContent: "center", fontSize: "2em", backgroundColor: "goldenrod", borderRadius: "50%", marginBottom: "0.5em", }}>{entry.icon}</p>
+            <p className="font-bold text-yellow-800" style={{ fontSize: "1.2em" }}>{entry.title}</p>
             <p className="leading-tight text-sm text-gray-600">
               {entry.description}
             </p>
